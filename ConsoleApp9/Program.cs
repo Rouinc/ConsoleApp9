0164e1dbd3afc4ff9fc0af7d4122d3b4204e781f
Rouinc <vincentalex9@outlook.com>
2021-12-03 10:19:18 +0700

    Mencoba

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+	}
+}
+
